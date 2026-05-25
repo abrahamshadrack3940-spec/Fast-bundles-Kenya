1a89ee802e1affd46120f745d0e61e0e80c6da0e
Abraham <abrahamshadrack3940@gmail.com>
2026-05-25 21:07:52 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function buyBundle(bundle, price) {
+  alert(
+    "You selected " +
+    bundle +
+    " worth KSH " +
+    price +
+    ".\n\nPay via Till Number: 3059265\n\nAfter payment send transaction code on WhatsApp: 0710372612"
+  );
+}
